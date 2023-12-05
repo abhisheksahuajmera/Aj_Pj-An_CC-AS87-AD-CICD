c2bf1b83f713071c5003b576229d4569e13638b8
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-05 16:06:03 +0530

    Resolve 101

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-// appusing-securing-adb2c
-module "securing_adb2c" {
-  source                  = "./modules/adb2c" // Add version after registry
-  country_code = local.resource_group_location_country_code
-  data_residency_location = local.resource_group_data_residency_location
-  resource_group_name     = local.resource_group_name
-}
-
 // appusing-storing-saaccount
 module "securing_saaccount" {
   source                  = "./modules/saaccount" // Add version after registry
