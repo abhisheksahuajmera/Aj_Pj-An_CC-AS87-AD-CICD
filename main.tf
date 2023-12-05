10c7bb599e93f272edf8325b6d2ca11dac644c27
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-05 16:22:40 +0530

    Try 100

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 // appusing-storing-saaccount
-module "securing_saaccount" {
-  source                  = "./modules/saaccount" // Add version after registry
-  resource_group_name     = local.resource_group_name
-  location     = local.resource_group_location
-}
+# module "securing_saaccount" {
+#   source                  = "./modules/saaccount" // Add version after registry
+#   resource_group_name     = local.resource_group_name
+#   location     = local.resource_group_location
+# }
 
-// appusing-slcomputing-serplan
-module "slcomputing_serplan" {
-  source              = "./modules/serplan" // Add version after registry
-  location     = local.resource_group_location
-  resource_group_name     = local.resource_group_name
-}
+# // appusing-slcomputing-serplan
+# module "slcomputing_serplan" {
+#   source              = "./modules/serplan" // Add version after registry
+#   location     = local.resource_group_location
+#   resource_group_name     = local.resource_group_name
+# }
 
-// appusing-slcomputing-azfunc
-module "slcomputing_azfunc" {
-  source              = "./modules/azfunc" // Add version after registry
-  location     = local.resource_group_location
-  resource_group_name     = local.resource_group_name
+# // appusing-slcomputing-azfunc
+# module "slcomputing_azfunc" {
+#   source              = "./modules/azfunc" // Add version after registry
+#   location     = local.resource_group_location
+#   resource_group_name     = local.resource_group_name
 
-  app_service_plan_id        = "${module.slcomputing_serplan.app_service_plan_idop}"
-  storage_account_name       = "${module.securing_saaccount.storage_account_nameop}"
-  storage_account_access_key = "${module.securing_saaccount.storage_account_access_keyop}"
-}
+#   app_service_plan_id        = "${module.slcomputing_serplan.app_service_plan_idop}"
+#   storage_account_name       = "${module.securing_saaccount.storage_account_nameop}"
+#   storage_account_access_key = "${module.securing_saaccount.storage_account_access_keyop}"
+# }
 
 
 // bootstraping-resourcing-rg
--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment_version" {
 variable "resource_group_version" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "104"
+  default     = "106"
 }
 
 variable "resource_group_prefix" {
