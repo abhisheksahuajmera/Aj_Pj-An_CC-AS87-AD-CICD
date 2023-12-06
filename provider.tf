7c4bf58ee43cec615e48a6a7733cb4a77eed2b1f
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-06 15:17:22 +0530

    Try 101

--- provider.tf
+++ provider.tf
@@ -16,7 +16,18 @@
 #   }
 # }
 
-provider "azurerm" {
-  features {}
-  use_oidc = true
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
