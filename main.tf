f2e4b322edbd98b7b5d8f41360279d76c3f907b7
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-06 11:43:24 +0530

    Pushed template

---
+++ main.tf
@@ -0,0 +1,47 @@
+// bootstraping-resourcing-rg
+resource "azurerm_resource_group" "resource_grouptg" {
+  name     = local.resource_group_name
+  location = local.resource_group_location
+}
+
+// appusing-storing-saaccount
+# module "securing_saaccount" {
+#   source                  = "./modules/saaccount" // Add version after registry
+#   resource_group_name     = local.resource_group_name
+#   location                = local.resource_group_location
+# }
+
+# // appusing-slcomputing-serplan
+# module "slcomputing_serplan" {
+#   source                  = "./modules/serplan" // Add version after registry
+#   location                = local.resource_group_location
+#   resource_group_name     = local.resource_group_name
+# }
+
+# // appusing-slcomputing-azfunc
+# module "slcomputing_azfunc" {
+#   source                     = "./modules/azfunc" // Add version after registry
+#   location                   = local.resource_group_location
+#   resource_group_name        = local.resource_group_name
+
+#   app_service_plan_id        = "${module.slcomputing_serplan.app_service_plan_idop}"
+#   storage_account_name       = "${module.securing_saaccount.storage_account_nameop}"
+#   storage_account_access_key = "${module.securing_saaccount.storage_account_access_keyop}"
+# }
+
+// orchestrating-containerizing-azacr
+# module "containerizing_azacr" {
+#   source                  = "./modules/azacr" // Add version after registry
+#   resource_group_name     = local.resource_group_name
+#   location                = local.resource_group_location
+# }
+
+// appusing-storing-azvault
+# module "containerizing_azvault" {
+#   source                  = "./modules/azvault" // Add version after registry
+#   resource_group_name     = local.resource_group_name
+#   location                = local.resource_group_location
+# }
+
+
+
