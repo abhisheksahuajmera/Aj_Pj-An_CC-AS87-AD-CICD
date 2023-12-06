7b512facfbb724bb02772af52420ec4547aeeac4
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-06 20:03:03 +0530

    Test 100

--- provider.tf
+++ provider.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name   = "${{var.state_resource_group_name}}"
-    storage_account_name  = "${{var.state_storage_account_name}}"
-    container_name        = "${{var.state_container_name}}"
-    key                   = "${{var.state_key}}"
-    access_key            = "${{var.state_access_key}}"
+    resource_group_name   = var.state_resource_group_name
+    storage_account_name  = var.state_storage_account_name
+    container_name        = var.state_container_name
+    key                   = var.state_key
+    access_key            = var.state_access_key
   }
 }
 
