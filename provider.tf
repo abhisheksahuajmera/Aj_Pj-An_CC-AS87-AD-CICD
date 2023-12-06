5b775d51bf1a975edc0fd79474c94c09af01c51d
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-06 16:18:06 +0530

    Try 100

--- provider.tf
+++ provider.tf
@@ -1,21 +1,3 @@
-# // DEV
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = ">= 3.7.0"
-#     }
-#   }
-
-#   backend "azurerm" {
-#     resource_group_name   = "Assetronai-dev-101-rg-108"
-#     storage_account_name  = "sa110"
-#     container_name        = "sacon100" 
-#     key                   = "state100.tfstate" 
-#     access_key            = "gM2VSx+n9bCQgm5GEhzMk2r4J8VpEib+3E9ORv+GaI0jATiOgyrK6ZIcaIEMwoy+40llPcFX9Pp/+AStSDJ+Cw=="
-#   }
-# }
-
 terraform {
   required_providers {
     azurerm = {
