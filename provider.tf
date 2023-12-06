dd578c6a78c0a91b298aeb255978d33516a3caee
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-06 15:13:01 +0530

    Try 100

--- provider.tf
+++ provider.tf
@@ -1,13 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 3.7.0"
+#     }
+#   }
+
+#   # Update this block with the location of your terraform state file
+#   backend "azurerm" {
+#     resource_group_name  = "rg-terraform-github-actions-state"
+#     storage_account_name = "terraformgithubactions"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#     use_oidc             = true
+#   }
+# }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
