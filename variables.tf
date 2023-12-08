11442f665dea47f510d6e5eb682dbadf8f817f83
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-08 09:22:07 +0530

    Try 200

--- variables.tf
+++ variables.tf
@@ -1,67 +1,56 @@
 variable "org" {
   description = "Org name"
   type        = string
-  default     = "Ajmerainfotech"
 }
 
 variable "project" {
   description = "project"
   type        = string
-  default     = "Aj_Pj-An_CC-AS87"
 }
 
 variable "project_prefix" {
   type        = string
   description = "(Optional) The prefix which should be used for all resources in this example. Defaults to burrito."
-  default     = "Assetronai"
 }
 
 variable "workspace" {
   description = "workspace"
   type        = string
-  default     = "Aj_Pj-An_CC-AS87-dev"
 }
 
 variable "environment" {
   description = "The name of the environment (ex. DEV, STAGING, PROD)"
   type        = string
-  default     = "dev"
 }
 
 variable "environment_version" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "100"
 }
 
 variable "state_resource_group_name" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "assetronai-dev-100-rg-102"
 }
 
 variable "state_storage_account_name" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "sa0999"
 }
 
 variable "state_container_name" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "sacon100"
 }
 
 variable "state_key" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "state101.tfstate"
 }
 
 variable "state_access_key" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "5v9vr4gJni1vbH7o/oNXXv5ocKtKeuenr/1kr27aRTjUQWb6LKfpHAszwILleSSAZTlqgEt8A60v+ASteZYaIw=="
 }
 
 variable "resource_group_version" {
